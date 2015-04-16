0af43d81e6f339d2cdb3a834df185271fbe453ef
Graham Mueller <graham@branchmetrics.io>
2015-04-16 15:28:53 -0500

    Breaking the default welcome view into its own view (trying to match the referral implementation). This removes a LOT of the code from the view controller; now it only dictates items to the view, which is nice.

---
+++ Classes/Welcome/BranchWelcomeDefaultView.m
@@ -0,0 +1,160 @@
+//
+//  BranchWelcomeDefaultView.m
+//  Pods
+//
+//  Created by Graham Mueller on 4/16/15.
+//
+//
+
+#import "BranchWelcomeDefaultView.h"
+#import "BranchInviteBundleUtil.h"
+#import "BranchInvite.h"
+
+CGFloat const PREFERRED_WIDTH = 288;
+
+@interface BranchWelcomeDefaultView ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *userImageView;
+@property (weak, nonatomic) IBOutlet UILabel *welcomeTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *welcomeBodyLabel;
+@property (weak, nonatomic) IBOutlet UIButton *cancelInviteButton;
+@property (weak, nonatomic) IBOutlet UIButton *confirmInviteButton;
+@property (weak, nonatomic) IBOutlet UILabel *earnedPointsLabel;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *cancelTopConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *userImageTopConstraint;
+
+@end
+
+@implementation BranchWelcomeDefaultView
+
+- (void)configureWithInviteUserInfo:(NSDictionary *)userInviteInfo {
+    self.userImageView.layer.masksToBounds = YES;
+    self.userImageView.layer.cornerRadius = self.userImageView.frame.size.width / 2.0;
+    self.userImageView.image = [BranchInviteBundleUtil imageNamed:@"user" type:@"png"];
+    
+    NSString *invitingUserFullname = userInviteInfo[BRANCH_INVITE_USER_FULLNAME_KEY];
+    NSString *invitingUserShortName = userInviteInfo[BRANCH_INVITE_USER_SHORT_NAME_KEY] ?: invitingUserFullname;
+    NSURL *invitingUserImageUrl = [NSURL URLWithString:userInviteInfo[BRANCH_INVITE_USER_IMAGE_URL_KEY]];
+    
+    if (invitingUserImageUrl) {
+        [self loadUserImage:invitingUserImageUrl];
+    }
+    
+    NSString *welcomeTitleText = [self welcomeTitleTextForFullname:invitingUserFullname shortName:invitingUserShortName];
+    NSString *welcomeBodyText = [self welcomeBodyTextForFullname:invitingUserFullname shortName:invitingUserShortName];
+    NSString *continueButtonText = [self continueButtonTextForFullname:invitingUserFullname shortName:invitingUserShortName];
+    
+    self.welcomeTitleLabel.text = welcomeTitleText;
+    self.welcomeBodyLabel.text = welcomeBodyText;
+    [self.confirmInviteButton setTitle:continueButtonText forState:UIControlStateNormal];
+    
+    // Versions prior to iOS 8.0 don't auto adjust label frames
+    if (NSFoundationVersionNumber <= NSFoundationVersionNumber_iOS_7_1) {
+        [self adjustTopConstaints];
+        [self resizeWelcomeTitleForText:welcomeTitleText];
+        [self resizeWelcomeBodyForText:welcomeBodyText];
+    }
+    
+    [self customizeAppearance];
+}
+
+- (UIButton *)cancelButton {
+    return self.cancelInviteButton;
+}
+
+- (UIButton *)continueButton {
+    return self.confirmInviteButton;
+}
+
+
+#pragma mark - Internals
+
+- (void)loadUserImage:(NSURL *)userImageUsl {
+    // Load the contents of this image asynchronously.
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSData *imageData = [NSData dataWithContentsOfURL:userImageUsl];
+        
+        // If the image fails to load, just leave the placeholder
+        if (!imageData) {
+            return;
+        }
+        
+        UIImage *invitingUserImage = [UIImage imageWithData:imageData];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.userImageView.image = invitingUserImage;
+        });
+    });
+}
+
+- (NSString *)welcomeTitleTextForFullname:(NSString *)invitingUserFullname shortName:(NSString *)invitingUserShortName {
+    NSString *normalizedWelcomeTitleText;
+    if ([self.delegate respondsToSelector:@selector(welcomeTitleTextForFullname:shortName:)]) {
+        normalizedWelcomeTitleText = [self.delegate welcomeTitleTextForFullname:invitingUserFullname shortName:invitingUserShortName];
+    }
+    else {
+        normalizedWelcomeTitleText = [NSString stringWithFormat:@"%@ has invited you to use this app!", invitingUserFullname];
+    }
+    
+    return normalizedWelcomeTitleText;
+}
+
+- (NSString *)welcomeBodyTextForFullname:(NSString *)invitingUserFullname shortName:(NSString *)invitingUserShortName {
+    NSString *normalizedWelcomeBodyText;
+    if ([self.delegate respondsToSelector:@selector(welcomeBodyTextForFullname:shortName:)]) {
+        normalizedWelcomeBodyText = [self.delegate welcomeBodyTextForFullname:invitingUserFullname shortName:invitingUserShortName];
+    }
+    else {
+        normalizedWelcomeBodyText = [NSString stringWithFormat:@"Welcome to the app! You've been invited to join the fun by another user, %@.", invitingUserShortName];
+    }
+    
+    return normalizedWelcomeBodyText;
+}
+
+- (NSString *)continueButtonTextForFullname:(NSString *)invitingUserFullname shortName:(NSString *)invitingUserShortName {
+    NSString *normalizedContinueButtonText;
+    if ([self.delegate respondsToSelector:@selector(welcomeContinueButtonTextForFullname:shortName:)]) {
+        normalizedContinueButtonText = [self.delegate welcomeContinueButtonTextForFullname:invitingUserFullname shortName:invitingUserShortName];
+    }
+    else {
+        normalizedContinueButtonText = [NSString stringWithFormat:@"Press to join %@", invitingUserShortName];
+    }
+    
+    return normalizedContinueButtonText;
+}
+
+- (void)customizeAppearance {
+    if ([self.delegate respondsToSelector:@selector(welcomeSchemeColor)]) {
+        UIColor *schemeColor = [self.delegate welcomeSchemeColor];
+        self.welcomeTitleLabel.textColor = schemeColor;
+        self.backgroundColor = schemeColor;
+        [self.cancelInviteButton setTitleColor:schemeColor forState:UIControlStateNormal];
+    }
+    
+    if ([self.delegate respondsToSelector:@selector(welcomeBodyTextColor)]) {
+        UIColor *textColor = [self.delegate welcomeBodyTextColor];
+        self.welcomeBodyLabel.textColor = textColor;
+        [self.confirmInviteButton setTitleColor:textColor forState:UIControlStateNormal];
+    }
+}
+
+- (void)adjustTopConstaints {
+    self.cancelTopConstraint.constant -= 16;
+    self.userImageTopConstraint.constant -= 20;
+}
+
+- (void)resizeWelcomeTitleForText:(NSString *)welcomeTitleText {
+    CGFloat height = [welcomeTitleText sizeWithFont:[UIFont boldSystemFontOfSize:20] constrainedToSize:CGSizeMake(PREFERRED_WIDTH, CGFLOAT_MAX)].height;
+    NSLayoutConstraint *heightConstraint = [NSLayoutConstraint constraintWithItem:self.welcomeTitleLabel attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:height];
+    
+    [self addConstraint:heightConstraint];
+}
+
+- (void)resizeWelcomeBodyForText:(NSString *)welcomeBodyText {
+    CGFloat height = [welcomeBodyText sizeWithFont:[UIFont systemFontOfSize:17] constrainedToSize:CGSizeMake(PREFERRED_WIDTH, CGFLOAT_MAX)].height;
+    NSLayoutConstraint *heightConstraint = [NSLayoutConstraint constraintWithItem:self.welcomeBodyLabel attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:height];
+    
+    [self addConstraint:heightConstraint];
+}
+
+@end
