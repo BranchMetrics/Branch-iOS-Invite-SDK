c5cae521ce1d035cf4823d82c65d084d6722793f
Graham Mueller <graham@branchmetrics.io>
2015-05-22 15:41:54 -0500

    Adding screen allowing you to view and redeem credits.

---
+++ Example/BranchInviteExample/CreditsController.h
@@ -0,0 +1,13 @@
+//
+//  CreditsController.h
+//  BranchInviteExample
+//
+//  Created by Graham Mueller on 5/22/15.
+//  Copyright (c) 2015 Branch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreditsController : UIViewController
+
+@end
